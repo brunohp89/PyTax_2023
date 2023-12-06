0b8ec0cabc51ce796a61efdba31c5143a644743a
Bruno Pinto <bruno.pinto.ext@adevinta.com>
2023-12-06 13:43:50 +0000

    NEAR calculator fixed with new data source

--- calculators/near.py
+++ calculators/near.py
@@ -3,183 +3,153 @@ from PricesClass import Prices
 import tax_library as tx
 import datetime as dt
 import pandas as pd
-import psycopg2
+import requests
+import json
+from dateutil.relativedelta import relativedelta
 
-conn = psycopg2.connect(
-    host="104.199.89.51",
-    database="mainnet_explorer",
-    user="public_readonly",
-    password="nearprotocol",
-)
-cur = conn.cursor()
+def get_transactions_df(address):
+    address = address.lower()
+    normal_df = pd.DataFrame()
+
+    url = f'https://api.nearblocks.io/v1/account/{address}/txns?page=1&per_page=25&order=desc'
+    trx_df = pd.DataFrame(json.loads(requests.get(url).content)['txns'])
+    normal_df = pd.concat([trx_df, normal_df])
+    i = 1
+    while trx_df.shape[0] > 0 and i < 150:
+        url = f'https://api.nearblocks.io/v1/account/{address}/txns?page={i}&per_page=25&order=desc'
+        trx_df = pd.DataFrame(json.loads(requests.get(url).content)['txns'])
+        normal_df = pd.concat([trx_df, normal_df])
+        i += 1
+
+    nep21_df = pd.DataFrame()
+    url = f'https://api.nearblocks.io/v1/account/{address}/ft-txns?page=1&per_page=25&order=desc'
+    trx_df = pd.DataFrame(json.loads(requests.get(url).content)['txns'])
+    nep21_df = pd.concat([trx_df, nep21_df])
+    i = 1
+    while trx_df.shape[0] > 0 and i < 150:
+        url = f'https://api.nearblocks.io/v1/account/{address}/ft-txns?page={i}&per_page=25&order=desc'
+        trx_df = pd.DataFrame(json.loads(requests.get(url).content)['txns'])
+        nep21_df = pd.concat([trx_df, nep21_df])
+        i += 1
+
+    normal_df = normal_df[~normal_df.transaction_hash.isin(nep21_df.transaction_hash)]
+    normal_df.index = [dt.datetime.fromtimestamp(int(int(x)/10**9)) for x in normal_df['block_timestamp']]
+    normal_df = normal_df.sort_index()
+    normal_df['Fee'] = [-int(k['transaction_fee'])/10**24 for k in normal_df['outcomes_agg']]
+    try:
+        normal_df['content'] = [json.loads(k[0].replace("\\","").replace("EVENT_JSON:","")) if len(k) > 0 else None for k in normal_df['logs']]
+    except:
+        normal_df['content'] = normal_df['logs']
+    normal_df['From'] = normal_df['predecessor_account_id']#[x['data'][0]['old_owner_id'] if x is not None else x for x in normal_df['content']]
+    normal_df['To'] = normal_df['receiver_account_id']#[x['data'][0]['new_owner_id'] if x is not None else x for x in normal_df['content']]
+
+    normal_df['amount'] = [k['deposit']/10**24 for k in normal_df['actions_agg']]
+    normal_df.loc[normal_df['predecessor_account_id'] == address, 'amount'] *= -1
+    normal_df.loc[normal_df['receiver_account_id'] == address, 'Fee'] *= 0
+    normal_df.loc[normal_df['amount'] < 0, 'From Amount'] = normal_df.loc[normal_df['amount'] < 0, 'amount']
+    normal_df.loc[normal_df['amount'] > 0, 'To Amount'] = normal_df.loc[normal_df['amount'] > 0, 'amount']
+
+    normal_df.loc[normal_df['amount'] < 0, 'From Coin'] = 'NEAR'
+    normal_df.loc[normal_df['amount'] > 0, 'To Coin'] = 'NEAR'
+
+    normal_df['Fee Coin'] = 'NEAR'
+    normal_df.loc[normal_df['Fee'] == 0,'Fee'] = None
+    normal_df['Tag'] = 'Movement'
+    normal_df['Notes'] = [str(x) for x in normal_df['actions']]
+    normal_df = normal_df.drop(['receipt_id', 'predecessor_account_id', 'receiver_account_id',
+       'transaction_hash', 'included_in_block_hash', 'block_timestamp',
+       'block', 'actions', 'actions_agg', 'outcomes', 'outcomes_agg', 'logs', 'content', 'amount'],axis=1)
+    normal_df['Fiat'] = 'EUR'
+    normal_df['Source'] = f'NEAR - {address[0:10]}'
+    normal_df[
+        ["Fee Fiat",
+        "Fiat Price"]
+    ] = None
+    normal_df = normal_df.drop_duplicates()
 
 
-def create_pandas_table(sql_query, database=conn):
-    table = pd.read_sql_query(sql_query, database)
-    return table
+    nep21_df.index = [dt.datetime.fromtimestamp(int(int(x) / 10 ** 9)) for x in nep21_df['block_timestamp']]
+    nep21_df = nep21_df.sort_index()
+    nep21_df.loc[nep21_df.token_old_owner_account_id==address,'Fee'] = -0.01 # Might be wrong but fee is not being transmitted
+    nep21_df['token'] = [x['name'] for x in nep21_df.ft]
+    nep21_df['decimals'] = [x['decimals'] for x in nep21_df.ft]
+    nep21_df['amount'] = [int(x)/(10**int(y)) for x,y in zip(nep21_df['amount'],nep21_df['decimals'])]
 
+    nep21_df.loc[nep21_df.token_old_owner_account_id==address, 'amount'] *= -1
+    nep21_df.loc[nep21_df.token_old_owner_account_id == 'jars.sweat', ['Tag', 'Notes']] = ['Reward','SWEAT Flexible Jars']
+    nep21_df.loc[nep21_df.token_new_owner_account_id == 'jars.sweat', 'amount'] = None
 
-def get_transactions_df(addresses_list):
-    addlist = addresses_list
-    addresses = "','".join(addlist)
-    addresses = f"'{addresses}'"
+    nep21_df.loc[nep21_df.token_old_owner_account_id == 'spin.sweat', ['Tag', 'Notes']] = ['Reward','SWEAT Spin Wheel']
+    nep21_df.loc[nep21_df.token_new_owner_account_id == 'spin.sweat', ['Tag', 'Notes']] = ['Movement', 'SWEAT Spin Wheel']
 
-    query_normal = f"""
-    SELECT block_timestamp as "timestamp", signer_account_id as "From", receiver_account_id as "To", action_kind as "kind", "args"
-    FROM TRANSACTIONS inner join transaction_actions ac on transactions.transaction_hash = ac.transaction_hash
-    WHERE signer_account_id in ({addresses})
-	OR receiver_account_id in ({addresses})
-	AND action_kind = 'TRANSFER'"""
+    nep21_df.loc[nep21_df['event_kind']=='MINT', ['Tag', 'Notes']] = ['Reward', 'SWEAT Steps Earn']
+    nep21_df.loc[nep21_df.token_new_owner_account_id == 'reward-optin.sweat', ['Tag', 'Notes']] = ['Movement', 'SWEAT Optin Prize']
+    nep21_df.loc[nep21_df.token_new_owner_account_id == 'fees.sweat', ['Tag', 'Notes']] = ['Movement', 'SWEAT Fees']
+    nep21_df.loc[nep21_df.token_new_owner_account_id == 'governance.sweat', ['Tag', 'Notes']] = ['Movement', 'SWEAT Governance Voting']
 
-    normal_transactions = create_pandas_table(query_normal)
+    jars_df = nep21_df[np.logical_or(nep21_df.token_new_owner_account_id == 'deposits.grow.sweat',nep21_df.token_old_owner_account_id == 'distributions.grow.sweat' )]
+    if jars_df.shape[0] > 0:
+        nep21_df.loc[np.logical_or(nep21_df.token_new_owner_account_id == 'deposits.grow.sweat',nep21_df.token_old_owner_account_id == 'distributions.grow.sweat' ), 'drop'] = 1
+        nep21_df = nep21_df[nep21_df['drop'] != 1]
+        jars_df.index = [k-dt.timedelta(seconds=k.second) for k in jars_df.index]
+        for optout in jars_df[jars_df.amount > 0].index:
+            one_month = optout - relativedelta(months=1)
+            three_months = optout - relativedelta(months=3)
+            six_months = optout - relativedelta(months=6)
+            twelve_months = optout - relativedelta(months=12)
+            if len(jars_df.loc[
+                       jars_df.index.isin([one_month - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount']) > 0:
+                jars_df.loc[jars_df.index == optout, 'amount'] = jars_df.loc[jars_df.index.isin([one_month-dt.timedelta(minutes=x) for x in range(-5,5)]), 'amount'].values[0] + jars_df.loc[jars_df.index == optout, 'amount'].values[0]
+                jars_df.loc[jars_df.index.isin([one_month-dt.timedelta(minutes=x) for x in range(-5,5)]), 'amount'] = None
 
-    normal_transactions = normal_transactions[normal_transactions["kind"] == "TRANSFER"]
-    normal_transactions["kind"] = "Movement"
+            if len(jars_df.loc[jars_df.index.isin([three_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount']) > 0:
+                jars_df.loc[jars_df.index == optout, 'amount'] = \
+                jars_df.loc[jars_df.index.isin([three_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount'].values[
+                    0] + jars_df.loc[jars_df.index == optout, 'amount'].values[0]
+                jars_df.loc[jars_df.index.isin([three_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount'] = None
 
-    normal_transactions["From"] = normal_transactions["From"].map(lambda x: x.lower())
-    normal_transactions["To"] = normal_transactions["To"].map(lambda x: x.lower())
+            if len(jars_df.loc[jars_df.index.isin(
+                        [six_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount']) > 0:
 
-    normal_transactions.index = normal_transactions["timestamp"].map(
-        lambda x: dt.datetime.fromtimestamp(int(x) / 10 ** 9)
-    )
+                jars_df.loc[jars_df.index == optout, 'amount'] = \
+                jars_df.loc[jars_df.index.isin([six_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount'].values[
+                    0] + jars_df.loc[jars_df.index == optout, 'amount'].values[0]
+                jars_df.loc[jars_df.index.isin([six_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount'] = None
 
-    normal_transactions["args"] = normal_transactions["args"].map(
-        lambda x: int(x["deposit"]) / 10 ** 24
-    )
+            if len(jars_df.loc[jars_df.index.isin(
+                        [twelve_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount']) > 0:
 
-    normal_transactions.rename(
-        columns={"args": "From Amount", "kind": "Tag"}, inplace=True
-    )
-    normal_transactions.drop(["timestamp"], axis=1, inplace=True)
+                jars_df.loc[jars_df.index == optout, 'amount'] = \
+                jars_df.loc[jars_df.index.isin([twelve_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount'].values[
+                    0] + jars_df.loc[jars_df.index == optout, 'amount'].values[0]
+                jars_df.loc[jars_df.index.isin([twelve_months - dt.timedelta(minutes=x) for x in range(-5, 5)]), 'amount'] = None
+        jars_df.loc[np.logical_or(jars_df['amount'] < 0,pd.isna(jars_df['amount'])), ['Tag','Notes']] = ['Movement', 'SWEAT Grow Jar']
+        jars_df.loc[jars_df['amount'] > 0, ['Tag', 'Notes']] = ['Reward', 'SWEAT Grow Jar']
+        jars_df.loc[jars_df['amount'] < 0, 'amount'] = None
 
-    normal_transactions["Fiat Price"] = None
-    normal_transactions["Fiat"] = "EUR"
-    normal_transactions["Fee Coin"] = "NEAR"
-    normal_transactions["Fee"] = None
-    normal_transactions["To Coin"] = None
-    normal_transactions["From Coin"] = "NEAR"
-    normal_transactions["To Amount"] = None
-    normal_transactions["Fee Fiat"] = None
-    normal_transactions["Tag"] = "Movement"
-    normal_transactions["Notes"] = None
-    normal_transactions["Source"] = "NEAR"
+        nep21_df = pd.concat([nep21_df,jars_df])
+        nep21_df = nep21_df.drop('drop',axis=1)
+    nep21_df.loc[nep21_df['amount'] < 0, 'From Amount'] = nep21_df.loc[nep21_df['amount'] < 0, 'amount']
+    nep21_df.loc[nep21_df['amount'] > 0, 'To Amount'] = nep21_df.loc[nep21_df['amount'] > 0, 'amount']
 
-    normal_transactions.loc[
-        np.logical_and(
-            normal_transactions["From"].isin(addlist),
-            ~normal_transactions["To"].isin(addlist),
-        ),
-        "From Amount",
-    ] *= -1
+    nep21_df.loc[nep21_df['amount'] < 0, 'From Coin'] = nep21_df.loc[nep21_df['amount'] < 0, 'token']
+    nep21_df.loc[nep21_df['amount'] > 0, 'To Coin'] = nep21_df.loc[nep21_df['amount'] > 0, 'token']
 
-    # Get NEP20 tokens
-    query_nep20 = f"""SELECT INCLUDED_IN_BLOCK_TIMESTAMP AS "timestamp",
-	AMOUNT,
-	EVENT_KIND,
-	TOKEN_NEW_OWNER_ACCOUNT_ID AS "To",
-	TOKEN_OLD_OWNER_ACCOUNT_ID AS "From",
-	RECEIPT_CONVERSION_GAS_BURNT AS "gas",
-	upper(substring(emitted_by_contract_account_id,7)) as "coin"
-    FROM ASSETS__FUNGIBLE_TOKEN_EVENTS AC
-    INNER JOIN RECEIPTS RP ON AC.EMITTED_FOR_RECEIPT_ID = RP.RECEIPT_ID
-    INNER JOIN TRANSACTIONS TRX ON TRX.TRANSACTION_HASH = RP.ORIGINATED_FROM_TRANSACTION_HASH
-    WHERE TOKEN_NEW_OWNER_ACCOUNT_ID in ({addresses})
-	OR TOKEN_OLD_OWNER_ACCOUNT_ID in ({addresses})"""
-    nep20_transactions = create_pandas_table(query_nep20)
+    nep21_df[['From','To']] = nep21_df[['token_old_owner_account_id', 'token_new_owner_account_id']]
+    nep21_df = nep21_df.drop(['key', 'token_old_owner_account_id', 'token_new_owner_account_id',
+       'amount', 'event_kind', 'transaction_hash', 'included_in_block_hash',
+       'block_timestamp', 'block', 'outcomes', 'ft', 'token',
+       'decimals'],axis=1)
+    nep21_df['Fiat'] = 'EUR'
+    nep21_df['Fee Coin'] = 'NEAR'
+    nep21_df['Source'] = f'NEAR - {address[0:10]}'
+    nep21_df[
+        ["Fee Fiat",
+        "Fiat Price"]
+    ] = None
+    nep21_df = nep21_df.drop_duplicates()
 
-    if nep20_transactions.shape[0] > 0:
-        nep20_transactions["From"] = nep20_transactions["From"].map(lambda x: x.lower())
-        nep20_transactions["To"] = nep20_transactions["To"].map(lambda x: x.lower())
-
-        #        nep20_transactions.loc[
-        #           nep20_transactions["To"] == "deposits.grow.sweat".lower(), "amount"
-        #      ] = 0  # Sweat staking
-
-        nep20_transactions["amount"] = [
-            int(s) / 10 ** 18 for s in nep20_transactions["amount"]
-        ]
-        nep20_transactions["gas"] = [
-            int(s) / 10 ** 18 for s in nep20_transactions["gas"]
-        ]
-
-        nep20_transactions.loc[
-            np.logical_and(
-                nep20_transactions["To"].isin(addlist),
-                nep20_transactions["From"].isin(addlist),
-                nep20_transactions["event_kind"] == "TRANSFER",
-            ),
-            "amount",
-        ] = 0
-
-        nep20_transactions.rename(
-            columns={
-                "from": "From",
-                "to": "To",
-                "amount": "From Amount",
-                "gas": "Fee",
-                "coin": "From Coin",
-                "event_kind": "Tag",
-            },
-            inplace=True,
-        )
-        nep20_transactions.index = nep20_transactions["timestamp"].map(
-            lambda x: dt.datetime.fromtimestamp(int(x) / 10 ** 9)
-        )
-        nep20_transactions.drop(["timestamp"], axis=1, inplace=True)
-
-        nep20_transactions.loc[
-            np.logical_and(
-                nep20_transactions["From"].isin(addlist),
-                ~nep20_transactions["To"].isin(addlist),
-                nep20_transactions["Tag"] == "TRANSFER",
-            ),
-            "From Amount",
-        ] *= -1
-
-        nep20_transactions["Fiat Price"] = None
-        nep20_transactions["Fiat"] = "EUR"
-        nep20_transactions["Fee Coin"] = "NEAR"
-        nep20_transactions["To Amount"] = None
-        nep20_transactions["To Coin"] = None
-        nep20_transactions.loc[
-            nep20_transactions["Tag"] == "TRANSFER", "Tag"
-        ] = "Movement"
-        nep20_transactions.loc[nep20_transactions["Tag"] == "MINT", "Tag"] = "Reward"
-        nep20_transactions.loc[
-            np.logical_and(
-                nep20_transactions["Tag"] == "Movement",
-                nep20_transactions["From"] == "tge-lockup.sweat",
-            ),
-            "Tag",
-        ] = "Reward"  # SWEAT airdrops
-        nep20_transactions["Fee Fiat"] = None
-        nep20_transactions["Notes"] = None
-        nep20_transactions["Source"] = "NEAR"
-
-        grow_jars = nep20_transactions.query("To == 'deposits.grow.sweat' or From == 'distributions.grow.sweat'").copy()
-        grow_jars['date'] = grow_jars.index.date
-
-        nep20_transactions = nep20_transactions.query("To != 'deposits.grow.sweat'").copy()
-        nep20_transactions = nep20_transactions.query("From != 'distributions.grow.sweat'").copy()
-
-        for i in grow_jars.query("To == 'deposits.grow.sweat'").index:
-            unlock_times = [(grow_jars.loc[[i], :].index + pd.Timedelta(days=x)).date[0] for x in [31, 91, 181, 366]]
-            unlock_times.append(i.date())
-            temp_df = grow_jars[grow_jars['date'].isin(unlock_times)].copy()
-            if temp_df.shape[0] > 0:
-                temp_df['From Amount'] = temp_df['From Amount'].sum()
-                temp_df = temp_df.query("From == 'distributions.grow.sweat'").copy()
-                temp_df.drop(['date'], axis=1, inplace=True)
-                temp_df['Tag'] = 'Reward'
-                temp_df['Notes'] = 'Growth Jar SWEAT'
-                nep20_transactions = pd.concat([nep20_transactions, temp_df])
-
-        outdf = pd.concat([nep20_transactions, normal_transactions])
-    else:
-        outdf = normal_transactions.copy()
-
-    outdf.sort_index(inplace=True)
-
+    outdf = pd.concat([normal_df, nep21_df]).sort_index()
     outdf = outdf[
         [
             "From",
@@ -198,11 +168,6 @@ def get_transactions_df(addresses_list):
             "Notes",
         ]
     ]
-    outdf["Fee"] *= -1
-    sub1 = outdf.loc[outdf["From Amount"] > 0, ["From Amount", "From Coin"]]
-    sub2 = outdf.loc[outdf["From Amount"] > 0, ["To Amount", "To Coin"]]
-    outdf.loc[outdf["From Amount"] > 0, ["To Amount", "To Coin"]] = sub1.values
-    outdf.loc[outdf["From Amount"] > 0, ["From Amount", "From Coin"]] = sub2.values
 
     outdf = tx.price_transactions_df(outdf, Prices())
     return outdf
